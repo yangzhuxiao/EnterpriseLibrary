aded733e757de35304a503c39064f5417c9c116e
Yang Xiaozhu <yangzhuxiao@126.com>
2015-01-25 14:47:43 +0800

    create books table view

---
+++ EnterpriseLibary/BooksTableViewController.m
@@ -0,0 +1,24 @@
+//
+//  AddBookViewController.m
+//  EnterpriseLibary
+//
+//  Created by Yang Xiaozhu on 1/25/15.
+//  Copyright (c) 2015 Xiaozhu-Jianing. All rights reserved.
+//
+
+#import "BooksTableViewController.h"
+
+@interface BooksTableViewController ()
+{
+    UIView *searchBarWrapperView;
+    UISearchController *searchController;
+}
+@end
+
+@implementation BooksTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
---
+++ EnterpriseLibary/LibraryInfoViewController.m
@@ -0,0 +1,17 @@
+//
+//  LibraryInfoViewController.m
+//  EnterpriseLibary
+//
+//  Created by Yang Xiaozhu on 1/25/15.
+//  Copyright (c) 2015 Xiaozhu-Jianing. All rights reserved.
+//
+
+#import "LibraryInfoViewController.h"
+
+@interface LibraryInfoViewController ()
+
+@end
+
+@implementation LibraryInfoViewController
+
+@end
