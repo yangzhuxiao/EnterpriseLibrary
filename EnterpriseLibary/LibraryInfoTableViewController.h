f833ac8ae188e7ea474869784aa8b3b480daa8a5
Yang Xiaozhu <yangzhuxiao@126.com>
2015-01-25 09:31:36 +0800

    added library info view

---
+++ EnterpriseLibary/LibraryInfoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LibraryInfoTableViewController.h
+//  EnterpriseLibary
+//
+//  Created by Yang Xiaozhu on 1/25/15.
+//  Copyright (c) 2015 Xiaozhu-Jianing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LibraryInfoTableViewController : UITableViewController
+
+@end
